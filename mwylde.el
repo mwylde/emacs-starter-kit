bda8329cdf71c63e617c1ea8118dafc3389c64f2
Micah Wylde <mwylde@wesleyan.edu>
2010-12-30 17:21:34 -0800

    Various additions I've found useful, including haskell-mode and coffee-mode

---
+++ mwylde.el
@@ -0,0 +1,48 @@
+;; Speed-bar in frame
+(load "~/.emacs.d/mwylde/sr-speedbar.el")
+(require 'sr-speedbar)
+(global-set-key (kbd "s-s") 'sr-speedbar-toggle)
+
+;; tab width
+(setq-default tab-width 4)
+
+;; Coffee-script support
+(add-to-list 'load-path "~/.emacs.d/mwylde/coffee-mode")
+(require 'coffee-mode)
+
+;; Haskell stuff
+(load "~/.emacs.d/mwylde/haskell-mode/haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;; Set color theme to twilight
+(color-theme-twilight)
+
+;; Originally from stevey, adapted to support moving to a new directory.
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive
+   (progn
+     (if (not (buffer-file-name))
+         (error "Buffer '%s' is not visiting a file!" (buffer-name)))
+     (list (read-file-name (format "Rename %s to: " (file-name-nondirectory
+                                                     (buffer-file-name)))))))
+  (if (equal new-name "")
+      (error "Aborted rename"))
+  (setq new-name (if (file-directory-p new-name)
+                     (expand-file-name (file-name-nondirectory
+                                        (buffer-file-name))
+                                       new-name)
+                   (expand-file-name new-name)))
+  ;; If the file isn't saved yet, skip the file rename, but still update the
+  ;; buffer name and visited file.
+  (if (file-exists-p (buffer-file-name))
+      (rename-file (buffer-file-name) new-name 1))
+  (let ((was-modified (buffer-modified-p)))
+    ;; This also renames the buffer, and works with uniquify
+    (set-visited-file-name new-name)
+    (if was-modified
+        (save-buffer)
+      ;; Clear buffer-modified flag caused by set-visited-file-name
+      (set-buffer-modified-p nil))
+  (message "Renamed to %s." new-name)))
