008af37d501867e12cce3226d73606c3d5b360c6
Micah Wylde <mwylde@wesleyan.edu>
2011-03-28 22:24:27 -0400

    Removed auto identation in haskell mode

--- mwylde.el
+++ mwylde.el
@@ -120,7 +120,7 @@ This is the same as using \\[set-mark-command] with the prefix argument."
            (and (not current-prefix-arg)
                 (member major-mode '(emacs-lisp-mode lisp-mode
                                                      clojure-mode    scheme-mode
-                                                     haskell-mode    ruby-mode
+                                                                     ruby-mode
                                                      rspec-mode      python-mode
                                                      c-mode          c++-mode
                                                      objc-mode       latex-mode
