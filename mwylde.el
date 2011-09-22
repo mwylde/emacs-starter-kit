b7b474e218bd2ed730ae736aa375910ce927b10a
mwylde <mwylde@wesleyan.edu>
2011-09-22 14:44:49 -0400

    Fixed flymake to use 1,9

--- mwylde.el
+++ mwylde.el
@@ -206,3 +206,17 @@ This is the same as using \\[set-mark-command] with the prefix argument."
 
 ;; Better c style
 (setq c-default-style "linux")
+
+;; Fix fly-make to use 1.9
+(if (file-exists-p "/Users/mwylde/.rvm/bin/ruby")
+    (setq flymake-ruby-command-name "/Users/mwylde/.rvm/bin/ruby")
+  (setq flymake-ruby-command-name "ruby"))
+
+(defun flymake-ruby-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    ;; Invoke ruby with '-c' to get syntax checking
+    (list flymake-ruby-command-name (list "-c" local-file))))
